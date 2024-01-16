ed95039e1d409a7fac3d5ebf118a378dba484422
India Kerle <india.kerle@nesta.org.uk>
2024-01-16 09:32:49 +0000

    add boilerplate code; change python version

---
+++ dap_job_quality/getters/ojo_getters.py
@@ -0,0 +1,71 @@
+"""
+Getters to retrieve OJO data from the database.
+"""
+from dap_job_quality import PRINZ_BUCKET_NAME
+from dap_job_quality.getters.data_getters import load_s3_data
+
+import os
+from typing import Dict, List
+import pandas as pd
+
+
+# Let's currently get a sample of the data from PRINZ to work with
+# for labelling etc.
+def get_ojo_sample() -> pd.DataFrame:
+    """Gets ojo sample data from s3
+
+    Returns:
+        pd.Dataframe: ojo sample data
+    """
+    return load_s3_data(
+        PRINZ_BUCKET_NAME,
+        "outputs/data/ojo_application/deduplicated_sample/ojo_sample.csv",
+    )
+
+
+def get_ojo_job_title_sample() -> pd.DataFrame:
+    """Gets ojo sample data with job title and sectors information from s3
+
+    Returns:
+        pd.Dataframe: ojo sample data
+    """
+    return load_s3_data(
+        PRINZ_BUCKET_NAME,
+        "outputs/data/ojo_application/deduplicated_sample/job_title_data_sample.csv",
+    )
+
+
+def get_ojo_location_sample() -> pd.DataFrame:
+    """Gets ojo sample data with location information from s3
+
+    Returns:
+        pd.Dataframe: ojo sample data
+    """
+    return load_s3_data(
+        PRINZ_BUCKET_NAME,
+        "outputs/data/ojo_application/deduplicated_sample/locations_data_sample.csv",
+    )
+
+
+def get_ojo_salaries_sample() -> pd.DataFrame:
+    """Gets ojo sample data with salaries information from s3
+
+    Returns:
+        pd.Dataframe: ojo sample data
+    """
+    return load_s3_data(
+        PRINZ_BUCKET_NAME,
+        "outputs/data/ojo_application/deduplicated_sample/salaries_data_sample.csv",
+    )
+
+
+def get_ojo_skills_sample() -> pd.DataFrame:
+    """Gets ojo sample data with skills information from s3
+
+    Returns:
+        pd.Dataframe: ojo sample data
+    """
+    return load_s3_data(
+        PRINZ_BUCKET_NAME,
+        "outputs/data/ojo_application/deduplicated_sample/skills_data_sample.csv",
+    )
