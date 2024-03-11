22d460bd6b4aefebd595a3e1645a4b354889c1c5
RFOxbury <rosamundoxbury@gmail.com>
2024-03-11 14:28:06 +0000

    Refactor functions and delete scrap code

--- dap_job_quality/utils/eda_utils.py
+++ dap_job_quality/utils/eda_utils.py
@@ -1,12 +1,15 @@
 """
 Helper functions for EDA steps that we might want to repeat
 """
-
+from collections import Counter
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-from typing import Union, List
-import pandas as pd
 import numpy as np
+import pandas as pd
+import re
+from typing import Union, List, Dict, Tuple
+from wordcloud import WordCloud
+
+from dap_job_quality.utils import text_cleaning as tc
 
 
 def make_wordclouds(
@@ -63,3 +66,73 @@ def make_wordclouds(
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     plt.savefig(output_dir)
     plt.show()
+
+
+def most_common_ngrams(
+    df: pd.DataFrame,
+    n: int = 2,
+    label_col: str = "label",
+    text_col: str = "labelled_span",
+    n_most_common: int = 10,
+) -> Dict[str, List[Tuple[str, int]]]:
+    """
+    Find the most common n-grams within each category in a dataframe.
+
+    This function assumes you have a dataframe where one column contains a labelled span of text,
+    and another column contains the label attributed to that span (eg "job design and nature of work", "other benefit"
+    etc). The function iterates over each category,
+    tokenizes the text in the corresponding text column, and finds the most common n-grams
+    within each category.
+
+    The aim is to find out what phrases come up most frequently in the context of sentences about categories like
+    "social support and cohesion" (eg "warm and friendly team", "supportive atmosphere" etc), "job design and
+    nature of work" (eg "ongoing training", "career progression" etc).
+
+    Args:
+        df (pd.DataFrame): The dataframe containing the text and labels.
+        n (int): The number of elements in each n-gram (default is 2).
+        label_col (str): The name of the column in the dataframe that contains the category labels (default is 'label').
+        text_col (str): The name of the column in the dataframe that contains the text to be tokenized (default is 'labelled_span').
+        n_most_common (int): The number of most common n-grams to return for each category (default is 10).
+
+    Returns:
+        Dict[str, List[Tuple[str, int]]]: A dictionary where the keys are the categories and the values are lists of tuples.
+        Each tuple contains an n-gram and its frequency, and the list is sorted by frequency in descending order.
+    """
+    category_ngrams = {}
+    for category in df[label_col].unique():
+        ngrams_list = []
+        for text in df[df[label_col] == category][text_col]:
+            n_grams = tc.tokenize(text, n)
+            ngrams_list.extend(n_grams)
+        category_ngrams[category] = Counter(ngrams_list).most_common(n_most_common)
+    return category_ngrams
+
+
+def find_phrase_and_sentence(text: str, phrases: List[str]) -> Tuple[bool, str]:
+    """
+    Searches for each phrase in a list within the provided text and returns the first sentence containing any phrase.
+
+    #TODO: extend this function so that it can return multiple sentences from the same job ad.
+
+    The function iterates through a list of phrases and checks if any of them are present in the text.
+    If a phrase is found, it returns a tuple with True and the sentence containing the phrase.
+    If none of the phrases are found in the text, it returns a tuple with False and an empty string.
+
+    Args:
+        text (str): The text in which to search for the phrases.
+        phrases (List[str]): A list of phrases to search for within the text.
+
+    Returns:
+        Tuple[bool, str]: A tuple where the first element is a boolean indicating if any phrase was found,
+        and the second element is the sentence containing the phrase. If no phrase is found, the second element is an empty string.
+    """
+    for phrase in phrases:
+        if phrase in text.lower():  # Check if the phrase is in the text
+            # Find the whole sentence containing the phrase
+            sentence = re.search(
+                r"([^.]*?" + re.escape(phrase) + r"[^.]*\.)", text, re.IGNORECASE
+            )
+            if sentence:
+                return True, sentence.group()
+    return False, ""
