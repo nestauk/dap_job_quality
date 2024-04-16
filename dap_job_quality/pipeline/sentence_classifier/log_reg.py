2dd4555061b5bfed44053b596ab8f74c39a89f6c
RFOxbury <rosamundoxbury@gmail.com>
2024-04-16 14:34:08 +0100

    Alter logreg hyperparams and reduce dimensionality

--- dap_job_quality/pipeline/sentence_classifier/log_reg.py
+++ dap_job_quality/pipeline/sentence_classifier/log_reg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 from sklearn.metrics import (
     classification_report,
     confusion_matrix,
@@ -11,6 +12,15 @@ import wandb
 
 from dap_job_quality import BUCKET_NAME, logging
 from dap_job_quality.getters.data_getters import load_s3_data
+
+LOG_REG_PARAMS = {
+    "penalty": "l2",
+    "solver": "liblinear",
+    "random_state": 42,
+    "max_iter": 100,
+}
+
+PCA_VAR = 0.95
 
 if __name__ == "__main__":
     X_train = load_s3_data(
@@ -37,12 +47,23 @@ if __name__ == "__main__":
         tags=[f"logistic_regression"],
     )
 
+    # Dimensionality Reduction with PCA
+    pca = PCA(n_components=PCA_VAR)  # retains 95% of the variance
+    X_train_pca = pca.fit_transform(X_train)
+    X_val_pca = pca.transform(X_val)
+    logging.info(X_train_pca.shape)
+
     # Train a logistic regression model
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+    model = LogisticRegression(
+        penalty=LOG_REG_PARAMS["penalty"],
+        solver=LOG_REG_PARAMS["solver"],
+        random_state=LOG_REG_PARAMS["random_state"],
+        max_iter=LOG_REG_PARAMS["max_iter"],
+    )
+    model.fit(X_train_pca, y_train)
 
     # Evaluate the model on the validation set
-    y_pred = model.predict(X_val)
+    y_pred = model.predict(X_val_pca)
     logging.info(classification_report(y_val, y_pred))
 
     wandb.run.summary["accuracy"] = accuracy_score(y_val, y_pred)
