3eb72c6d520b8e7ddfa6ffaf6ddfa4cbc1da0c59
lizgzil <lizgzil@hotmail.com>
2024-06-13 15:12:41 +0100

    revert to previous replacements and add Rosie's change too

--- dap_job_quality/utils/text_cleaning.py
+++ dap_job_quality/utils/text_cleaning.py
@@ -109,16 +109,10 @@ def replacements(text):
     text = (
         text.replace("&", "and")
         .replace("\xa0", " ")
-        .replace("\n", "")
+        .replace("\r", ".")
+        .replace("\n", ".")
         .replace("[", "")
         .replace("]", "")
-        .replace("onc", "1")
-        .replace("two", "2")
-        .replace("three", "3")
-        .replace("four", "4")
-        .replace("five", "5")
-        .replace("six", "6")
-        .replace("seven", "7")
     )
 
     for pattern, rep in compiled_punct_patterns.items():
