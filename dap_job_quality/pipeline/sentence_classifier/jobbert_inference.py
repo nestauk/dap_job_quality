2d541ff740b2f3b222c3f2ab2549d758a92a127d
RFOxbury <rosamundoxbury@gmail.com>
2024-07-11 15:40:42 +0100

    Update inference code: test how long it takes to run

--- dap_job_quality/pipeline/sentence_classifier/jobbert_inference.py
+++ dap_job_quality/pipeline/sentence_classifier/jobbert_inference.py
@@ -1,15 +1,64 @@
 """
 This script shows how to use the fine-tuned jobbert model for inference.
+
+Usage:
+```
+python dap_job_quality/pipeline/sentence_classifier/jobbert_inference.py --input_text="You will receive £1 million per hour."
+```
 """
 import argparse
 import logging
+import nltk
+from nltk.tokenize import sent_tokenize
+import pandas as pd
+from time import time
 import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from typing import List, Union, Tuple
 
 from dap_job_quality import PROJECT_DIR, BUCKET_NAME
+from dap_job_quality.getters.afs_data import get_stratified_sample
 from dap_job_quality.getters.data_getters import download_and_extract_from_s3
 
 MODEL_DIR_LOCAL = PROJECT_DIR / "outputs/data/models/"
+
+nltk.download("punkt")
+nltk.download("stopwords")
+
+
+def extract_job_quality_sentences(
+    job_adverts: Union[pd.DataFrame, List[str], str],
+    id_col: str = "id",
+    text_col: str = "clean_description",
+) -> List[str]:
+    """Convert job advert dataframe to list of texts to use for inference.
+
+    Args:
+        job_adverts (Union[pd.DataFrame, List[str], str]): Dataframe of job ads
+        id_col (str, optional): Unique identifier of each ad. Defaults to "id".
+        text_col (str, optional): Name of the column containing the job description. Defaults to "clean_description".
+
+    Returns:
+        List[str]: List of sentences from job adverts.
+    """
+    if isinstance(job_adverts, str):
+        jobs_df = pd.DataFrame([{id_col: 0, text_col: job_adverts}])
+    elif isinstance(job_adverts, list):
+        jobs_df = (
+            pd.DataFrame(job_adverts, columns=[text_col])
+            .reset_index()
+            .rename(columns={"index": id_col})
+        )
+    elif isinstance(job_adverts, pd.DataFrame):
+        jobs_df = job_adverts[[id_col, text_col]].copy()
+    else:
+        raise ValueError("Input must be a DataFrame, a list of strings, or a string")
+
+    jobs_df["sentences"] = jobs_df[text_col].apply(lambda x: sent_tokenize(x))
+    jobs_df = jobs_df.explode("sentences")
+
+    return jobs_df["sentences"].tolist()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -17,14 +66,26 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--input_text",
-        default="You will receive £1 million per hour.",
-        type=str,
-        help="Text to classify",
+        "--n_samples",
+        default=1,
+        type=int,
+        help="Number of samples to test the pipeline on",
     )
 
     args = parser.parse_args()
     logging.info(args)
+
+    logging.info("Loading data...")
+    job_adverts = get_stratified_sample()
+
+    if args.n_samples <= len(job_adverts):
+        job_adverts = job_adverts.head(args.n_samples)
+    else:
+        logging.info(
+            f"Number of samples requested exceeds the total number of samples. Returning all {len(job_adverts)} samples."
+        )
+
+    input_texts = extract_job_quality_sentences(job_adverts)
 
     logging.info("Downloading the model...")
     download_and_extract_from_s3(
@@ -40,24 +101,31 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR_LOCAL / "jobbert-base-cased-jq")
 
     # Tokenize the new data
-    logging.info("Tokenizing the input text...")
-    inputs = tokenizer(
-        args.input_text,
-        truncation=True,
-        padding=True,
-        return_tensors="pt",
-        max_length=128,
+    logging.info("Classifying texts...")
+    start_time = time()
+
+    classifier = pipeline(
+        "text-classification",
+        model=model,
+        tokenizer=tokenizer,
+        return_all_scores=False,
+        device=0 if torch.cuda.is_available() else -1,  # Use GPU if available
     )
 
-    logging.info("Making predictions...")
-    model.eval()
-    with torch.no_grad():
-        outputs = model(**inputs)
-        predictions = torch.argmax(outputs.logits, dim=-1)
+    predictions = classifier(input_texts)
 
-    logging.info("Converting predictions to labels...")
-    predicted_labels = predictions.numpy()
+    end_time = time()
 
     logging.info(
-        f"Input text: {args.input_text} \n \n Predicted label: {predicted_labels}"
+        f"Time taken for inference on {len(job_adverts)} texts: {end_time - start_time} seconds"
+    )
+
+    logging.info("Converting predictions to labels...")
+    predicted_labels = [pred["label"] for pred in predictions]
+
+    logging.info("Saving predictions to CSV...")
+    df = pd.DataFrame({"input_text": input_texts, "predicted_label": predicted_labels})
+    df.to_csv(
+        PROJECT_DIR / f"outputs/data/jobbert_test_n_samples_{args.n_samples}.csv",
+        index=False,
     )
