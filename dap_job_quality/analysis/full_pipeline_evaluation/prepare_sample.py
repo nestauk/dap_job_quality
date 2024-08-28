426d780ce5141170540133f4fa8d1649638cc328
RFOxbury <rosamundoxbury@gmail.com>
2024-08-28 08:49:40 +0100

    Update how sample is created

--- dap_job_quality/analysis/full_pipeline_evaluation/prepare_sample.py
+++ dap_job_quality/analysis/full_pipeline_evaluation/prepare_sample.py
@@ -1,5 +1,6 @@
 from dap_job_quality.getters.ojo_getters import get_ojo_job_title_sample, get_ojo_sample
 from dap_job_quality.utils.text_cleaning import clean_text
+from dap_job_quality import logging
 
 import nltk
 from nltk.tokenize import sent_tokenize
@@ -8,16 +9,22 @@ import pandas as pd
 if __name__ == "__main__":
 
     # Load the sentence classifier test data, plus the sector and job descriptions data
-    test_data = pd.read_parquet(
-        "s3://open-jobs-lake/job_quality/sentence_classifier/inputs/labelled/test_df_20240725.parquet"
+    val_ids = pd.read_parquet(
+        "s3://open-jobs-lake/job_quality/sentence_classifier/inputs/labelled/val_ids_20240725.parquet"
     )
+    train_ids = pd.read_parquet(
+        "s3://open-jobs-lake/job_quality/sentence_classifier/inputs/labelled/train_ids_20240725.parquet"
+    )
+    train_val = pd.concat(
+        [train_ids.drop_duplicates("id"), val_ids.drop_duplicates("id")]
+    )
+
     ojo_sector_data = get_ojo_job_title_sample()
     ojo_desc = get_ojo_sample()
     ojo_sample = ojo_sector_data.merge(
         ojo_desc[["id", "description", "itl_3_code", "itl_3_name"]], on="id", how="left"
     )
 
-    # Get some more data for key EY careers (this is underrepresented in the sentence classifier test dataset)
     extra_eyp = (
         ojo_sample[
             ojo_sample["sector"].isin(
@@ -35,10 +42,17 @@ if __name__ == "__main__":
     )
     extra_eyp["origin"] = "extra_eyp"
 
-    # Merge together
-    eval_data = ojo_sample[ojo_sample["id"].isin(test_data["id"])]
-    eval_data["origin"] = "sent_class_test"
+    eval_data = ojo_sample[ojo_sample["id"].isin(train_val["id"])]
+    eval_data["origin"] = "sent_class_val"
     eval_data = pd.concat([eval_data, extra_eyp])
+
+    # Sample so that you get a maximum of 5 ads per parent_sector
+    sampled_df = eval_data.groupby("parent_sector", group_keys=False).apply(
+        lambda x: x.sample(min(len(x), 5))
+    )
+
+    sampled_df = sampled_df.reset_index(drop=True)
+    logging.info(f"Number of ads in sample: {len(sampled_df)}")
 
     # Separate by sentence
     eval_data["clean_description"] = (
@@ -55,6 +69,6 @@ if __name__ == "__main__":
 
     # Save
     eval_data_per_sentence.to_csv(
-        "s3://open-jobs-lake/job_quality/outputs/evaluation/evaluation_data.csv",
+        "s3://open-jobs-lake/job_quality/outputs/evaluation/evaluation_data_20240827.csv",
         index=False,
     )
